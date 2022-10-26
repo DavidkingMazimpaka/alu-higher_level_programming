2ba3df4391f49d8b6af89558dea1d02cafaf0b38
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-10-26 11:14:52 -0700

    if with random numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,22 +4,15 @@ number = random.randint(-10000, 10000)
 def digits(args):
     if args >= 0:
         x = args % 10
-        
         if x > 5:
-            
-            print(f"last digit of {args} is {x} and is greater than 5")
-            
+            print(f"Last digit of {args} is {x} and is greater than 5")
         elif x == 0:
-            
-            print(f"last digit of {args} is {x} and is 0")
-            
+            print(f"Last digit of {args} is {x} and is 0")
         elif x < 6 & x != 0:
-            
-            print(f"last digit of {args} is {x} and is less than 6 and not 0")
-            
+            print(f"Last digit of {args} is {x} and is less than 6 and not 0"
     else:
         x = -args % 10
         y = -x
-        print(f"last digit of {args} is {y} and is less than 6 and not 0")
+        print(f"Last digit of {args} is {y} and is less than 6 and not 0")
         
 digits(number)
