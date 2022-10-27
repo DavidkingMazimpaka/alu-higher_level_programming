ac5573a6adaf400cc7a56dd078bd1b0aa8c22012
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-10-27 12:58:46 -0700

    print: last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,4 @@
 def print_last_digit(number):
     num_str = repr(number)
     last_num = num_str[-1]
-    print(f"The last digit of {number} is {last_num}")
-print_last_digit(2334)
+    return last_num
