bb509061898df6b234842328721d30820579f7ba
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-10-28 05:21:48 -0700

    hidden discovery program

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 import hidden_4
 if __name__ == "__main__":
     for j in dir(hidden_4):
-        if not j.startswith('__')
-        print(j)
+        if not j.startswith('__'):
+            print(j)
