47061ad84d394ba2346dc30ee6cc61a4b7323235
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-03 05:10:50 -0700

    pass Cc

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    word = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'C' or my_string[i] == 'c':
+            pass
+        else:
+            word += my_string[i]
+
+    return word
