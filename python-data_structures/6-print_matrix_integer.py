5f03656493a8815a86af5dd2dcd1bf5f7062b1a6
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-03 06:43:08 -0700

    py:matrix[]

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for j in matrix:
-        a = 0
-        for i in j:
-            if a == (len(j) - 1):
-                print("{:d}".format(i), end="")
-            else:
-                print("{:d}".format(i), end="")
-            a += 1
-        print(" ")
+        if not j:
+            print()
+        else:
+            for i in j:
+                if i == j[-1]:
+                    print("{:d}".format(i))
+                else:
+                    print('{:d}'.format(i), end=" ")
