af2f67dbb8039e9ffb44dfc64b7f648e24fa3645
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-03 10:43:23 -0700

    py:tuple_addition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -12,6 +12,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b += (0,)
     elif len(tuple_b) > 2:
         tuple_b = tuple_b[:2]
-    1st_addition = tuple_a[0] + tuple_b[0]
-    2nd_addition = tuple_a[1] + tuple_b[1]
-    reutn tuple((1st_addition, 2nd_addition))
+    first_element = tuple_a[0] + tuple_b[0]
+    second_element = tuple_a[1] + tuple_b[1]
+    return tuple((first_element, second_element))
