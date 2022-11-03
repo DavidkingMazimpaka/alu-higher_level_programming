3d66ed6c429ba8cd17fa58dd2690c07271e7733b
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-03 11:00:54 -0700

    multiple returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     if sentence:
         length = len(sentence)
-        1st_letter = sentence[0]
+        fst_letter = sentence[0]
     else:
         length = 0
-        1st_letter = None
-    return tuple((length, 1st_letter))
+        fst_letter = None
+    return tuple((length, fst_letter))
