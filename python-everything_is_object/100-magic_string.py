1c4b61c768092a9dc43322d8347117e061aefe66
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-16 14:15:00 +0200

    py: advanced task mutable file

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def magic_string(str_i=None):
-    if str_i is None:
-        str_i = []
+def magic_string(str_i=[]):
     str_i .append("BestSchool")
     return ", ".join(str_i)
