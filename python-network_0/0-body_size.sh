d793d981d12ddf3266f1ea93a9b1f80a3840e93e
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-18 21:48:43 +0200

    py: cURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#content length in a header
-curl -sI "$1" | grep "Content-Length" | cut -d ":" -f2
+# send request to URL and determine the size of the body
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
