6d1b3aab5272d26dd2d82f354d3ed7f884356c8f
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-22 11:14:47 +0200

    py: check status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-This file checks and fetches the status of current url from the server
-"""
-from urllib.request import urlopen
+"""fetching an url"""
 
+import urllib.request
 
-if __name__ == "__main__":
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        body = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode()))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+
