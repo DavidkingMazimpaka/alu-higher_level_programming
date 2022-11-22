99e762b6ae1216c73ec3f588ddcee01ea8930dc4
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-22 12:06:37 +0200

    py: check status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.urlopen('https://alu-intranet.hbtn.io/status') as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode()))
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
