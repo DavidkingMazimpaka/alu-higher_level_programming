7a2ebde597cd7ffe4054281dd3fb434c45c68f75
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-22 13:12:20 +0200

    py:network_1 check status#4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Fetch URL """
+"""Fetch a url"""
+
 import requests
 
 
 if __name__ == "__main__":
-    reply = requests.get('https://alu-intranet.hbtn.io/status').text
+    response = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(reply)))
-    print("\t- content: {}".format(reply))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
