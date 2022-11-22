94e6ae6679f0174442a65d15142ecacc45b331e9
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-11-22 15:09:40 +0200

    py: advanced task Time for an interview!

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -3,20 +3,13 @@
 import sys
 import requests
 
-
-def apidata():
-    """apidata"""
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
-                                                              sys.argv[1])
-    result = requests.get(url)
-    try:
-        d = result.json()
-        for i in range(10):
-            print("{}: {}".format(d[i]["sha"],
-                                  d[i]["commit"]["author"]["name"]))
-    except IndexError:
-        pass
-
-
 if __name__ == "__main__":
-    apidata()
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    print(url)
+    commits = requests.get(url).json()
+
+    for data in commits:
+        print("{}: {}".format(data['sha'], data['author']['login']))
