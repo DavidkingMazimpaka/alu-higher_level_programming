6234cbdf43b0ece36f6c24f173da3b4f9f5618e7
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-12-13 20:22:36 +0200

    py:class with states

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,16 @@
-class Base:
-    metadata = None
+#!/usr/binpython3
+"""
+class definition and instances
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class State:
-    name = None
-    id = None
+class State(Base):
+    """Class for State"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
