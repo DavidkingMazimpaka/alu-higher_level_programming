5e6fc8c649e3d2c756ba597cf12d38bd66a50bf7
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-12-13 17:07:43 +0200

    py:all in square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,78 +1,60 @@
 #!/usr/bin/python3
-"""
-class Square
-"""
-from .rectangle import Rectangle
+"""model `Square`"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    creating class Square inheriting parent class Rectangle
-    """
+    """class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        init class constructor with initiated parameters
-        :param size:
-        :param x:
-        :param y:
-        :param id:
-        """
+        """constructor"""
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """
-        returning another attrib
-        :return:
-        """
-        attributes = "[Square] ({}) {}/{} - {}"
-        return attributes.format(self.id, self.x, self.y, self.width)
+        self.y = None
+        self.x = None
+        self.id = None
+        self.height = None
+        self.width = None
 
     @property
     def size(self):
-        """
-        getter method 'size'
-        :return:
-        """
-        return self.width
+        """size getter"""
+        return super().width
 
     @size.setter
     def size(self, value):
-        """
-        setter for method 'size'
-        :param value:
-        :return:
-        """
+        """size setter"""
         self.width = value
         self.height = value
 
+    def __str__(self):
+        """return a string with the data passed"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+
     def update(self, *args, **kwargs):
-        """
-        assigning parameters to method
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        if len(args):
-            i = 0
-            keys_of_attributes = ['id', 'size', 'x', 'y']
-            for value in args:
-                if i < 4:
-                    setattr(self, keys_of_attributes[i], value)
-                    i += 1
-        else:
-            for keys_of_attributes, value in kwargs.items():
-                setattr(self, keys_of_attributes, value)
+        """update the attributes"""
+        if len(args) > 0:
+            if args[0] is not None:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+                self.height = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        elif len(kwargs) > 0:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.height = kwargs['size']
+                self.width = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
 
     def to_dictionary(self):
-        """
-        returning a dictionary attrib
-        :return:
-        """
-        dic = {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-        }
-        return dic
+        """returns the dictionary representation of a Square"""
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'size': self.width}
