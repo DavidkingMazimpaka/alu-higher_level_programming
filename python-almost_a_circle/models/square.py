6b3146e17200f4eb6c01b658e94844b9f6d40a32
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-12-13 17:11:02 +0200

    py:all in square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,60 +1,52 @@
 #!/usr/bin/python3
-"""model `Square`"""
-from models.rectangle import Rectangle
+"""
+class Square
+"""
+from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class Square"""
+    """ square class """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """constructor"""
+        """ class constructor"""
         super().__init__(size, size, x, y, id)
-        self.y = None
-        self.x = None
-        self.id = None
-        self.height = None
-        self.width = None
+
+    def __str__(self):
+        """ str attribute """
+        attributes = "[Square] ({}) {}/{} - {}"
+        return attributes.format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """size getter"""
-        return super().width
+        """ Getter of the size attribute """
+        return self.width
 
     @size.setter
     def size(self, value):
-        """size setter"""
+        """ setter for size """
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """return a string with the data passed"""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
-
     def update(self, *args, **kwargs):
-        """update the attributes"""
-        if len(args) > 0:
-            if args[0] is not None:
-                self.id = args[0]
-            if len(args) > 1:
-                self.width = args[1]
-                self.height = args[1]
-            if len(args) > 2:
-                self.x = args[2]
-            if len(args) > 3:
-                self.y = args[3]
-        elif len(kwargs) > 0:
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            if 'size' in kwargs:
-                self.height = kwargs['size']
-                self.width = kwargs['size']
-            if 'x' in kwargs:
-                self.x = kwargs['x']
-            if 'y' in kwargs:
-                self.y = kwargs['y']
+        """ assign the  attributes """
+        if len(args):
+            i = 0
+            keys_of_attributes = ['id', 'size', 'x', 'y']
+            for value in args:
+                if i < 4:
+                    setattr(self, keys_of_attributes[i], value)
+                    i += 1
+        else:
+            for keys_of_attributes, value in kwargs.items():
+                setattr(self, keys_of_attributes, value)
 
     def to_dictionary(self):
-        """returns the dictionary representation of a Square"""
-        return {'x': self.x, 'y': self.y, 'id': self.id,
-                'size': self.width}
+        """ square dictionary """
+        dic = {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+            }
+        return dic
