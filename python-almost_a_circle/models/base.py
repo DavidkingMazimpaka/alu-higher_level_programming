42ae794d0297c2f5b2d7af8d787df0dd9d1ec635
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2022-12-15 00:37:50 +0200

    commiting the changes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,153 +1,96 @@
 #!/usr/bin/python3
-"""Module definition"""
+"""Class Base"""
 
 import json
-import turtle
-import csv
 
 
 class Base:
-    """Create Base class"""
+    """"
+        Class Base
+        Attr :
+                id: number
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new id"""
-        if id is not None:
-            self.id = id
+        Base.__nb_objects += 1
+        self.id = id
+
+    @property
+    def id(self):
+        """Doc"""
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        """Doc"""
+        if value is None:
+            self.__id = self.__nb_objects
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.__id = value
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Convert a list of dictionaries to a JSON string"""
-        if list_dictionaries is None:
+        """Doc"""
+        if list_dictionaries is None or \
+                len(list_dictionaries) == 0:
             return "[]"
-
-        if len(list_dictionaries) == 0:
-            return "[]"
-
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Save a list of objects to a file"""
-        file_name = cls.__name__ + ".json"
-        new_list = []
-        if list_objs:
-            for i in list_objs:
-                new_list.append(cls.to_dictionary(i))
-
-        with open(file_name, mode="w") as myFile:
-            myFile.write(cls.to_json_string(new_list))
+        """writes the JSON string representation
+        of list_objs to a file
+        """
+        list_objs_dict = []
+        with open(cls.__name__ + '.json', "w") as file:
+            if list_objs is None or len(list_objs) == 0:
+                file.write("[]")
+            elif type(list_objs) == list:
+                for obj in list_objs:
+                    list_objs_dict.append(obj.to_dictionary())
+                file.write(cls.to_json_string(list_objs_dict))
 
     @staticmethod
     def from_json_string(json_string):
-        """Convert a JSON string to a list of dictionaries"""
-        if json_string is None:
-            return []
-
-        if len(json_string) == 0:
-            return []
-
-        list_dicts = json.loads(json_string)
-        return list_dicts
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or \
+                len(json_string) == 0:
+            return list()
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Create a new object"""
-        global dummy
+        """ returns an instance with all attributes already set"""
+        global dummy_instance
         if cls.__name__ == "Rectangle":
-            dummy = cls(3)
+            dummy_instance = cls(4, 3)
         if cls.__name__ == "Square":
-            dummy = cls(3)
-        dummy.update(**dictionary)
-        return dummy
+            dummy_instance = cls(4)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
 
     @classmethod
     def load_from_file(cls):
-        """Load a list of objects from a file"""
+        """returns a list of instances from file"""
         try:
             with open(cls.__name__ + ".json", "r") as file:
-                content = file.read()
+                serialized_content = file.read()
         except FileNotFoundError:
-            return []
+            return list()
 
-        ex_content = cls.from_json_string(content)
-        context_list = []
-        for instance_dict in ex_content:
-            context_list.append(cls.create(**instance_dict))
-        return context_list
+        deserialized_content = cls.from_json_string(serialized_content)
 
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """ Save to a CSV file """
-        fn = cls.__name__ + ".csv"
-        if fn == "Rectangle.csv":
-            fields = ["id", "width", "height", "x", "y"]
-        else:
-            fields = ["id", "size", "x", "y"]
-        with open(fn, mode="w", newline="") as myFile:
-            if list_objs is None:
-                writer = csv.writer(myFile)
-                writer.writerow([[]])
-            else:
-                writer = csv.DictWriter(myFile, fieldnames=fields)
-                writer.writeheader()
-                for x in list_objs:
-                    writer.writerow(x.to_dictionary())
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """ Load from a CSV file """
-        try:
-            fn = cls.__name__ + ".csv"
-            with open(fn, newline="") as myFile:
-                reader = csv.DictReader(myFile)
-                lst = []
-                for x in reader:
-                    for i, n in x.items():
-                        x[i] = int(n)
-                    lst.append(x)
-                return [cls.create(**objet) for objet in lst]
-        except FileNotFoundError:
-            return []
-
-    @staticmethod
-    def draw(list_rectangles, list_squares):
-        """ Draw the rectangles and squares """
-        shapes = []
-        if list_rectangles:
-            shapes.extend(list_rectangles)
-        if list_squares:
-            shapes.extend(list_squares)
-        pen = turtle.Turtle()
-        pen.pen(pencolor='black', pendown=False, pensize=2, shown=False)
-        for shape in shapes:
-            pen.penup()
-            pen.setpos(shape.x, shape.y)
-            pen.pendown()
-            pen.forward(shape.width)
-            pen.right(90)
-            pen.forward(shape.height)
-            pen.right(90)
-            pen.forward(shape.width)
-            pen.right(90)
-            pen.forward(shape.height)
-            pen.right(90)
-
-    @classmethod
-    def to_dictionary(cls, i):
-        """
-        to pass cls an i args method
-        :param i:
-        :return:
-        """
-        pass
+        instances_list = []
+        for instance_dict in deserialized_content:
+            instances_list.append(cls.create(**instance_dict))
+        return instances_list
 
     def update(self, param):
         """
-        updating method
+        update method
         :param param:
         :return:
         """
