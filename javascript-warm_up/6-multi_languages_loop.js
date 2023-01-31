0f436d04800379e70580c79702f2ce4458eac5c7
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-01-31 18:10:17 +0200

    final commit

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const langs = ['C is fun','Python is cool','Javascript is amazing'];
-for (j=0; j < langs.length; j++){
-console.log(langs[j]);
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
