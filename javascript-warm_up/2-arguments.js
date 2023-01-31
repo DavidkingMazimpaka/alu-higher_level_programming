f5598b99f2cbe2c1ccd8cfc21986d98bc0fdcbd2
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-01-31 18:06:44 +0200

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2){
-console.log ('No argument');
+const long = process.argv.length - 2;
+if (long <= 0) {
+  console.log('No argument');
+} else if (long === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (process.argv.length === 3){
-console.log ('Argument found');
-}
-else {
-console.log ('Arguments found');
-}
-
